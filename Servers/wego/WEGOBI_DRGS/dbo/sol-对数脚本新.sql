34d924f541dc3155559c0456057e4dbb3a97dd02
Ysongcao <2804119807@qq.com>
2025-04-15 13:33:06 +0800

    修改对数脚本

--- Servers/wego/WEGOBI_DRGS/dbo/sol-对数脚本新.sql
+++ Servers/wego/WEGOBI_DRGS/dbo/sol-对数脚本新.sql
@@ -1,20 +1,15 @@
-DECLARE @startTime datetime,@endTime datetime ;
-set @startTime ='2025-03-01';
-set @endTime ='2025-03-31';
-select  @startTime,@endTime
-
 --1.初始化
-update t_setlinfo   set  isdrg=0   where  convert(date,brjsrq,23)  between  @startTime  and  @endTime;
+update t_setlinfo   set  isdrg=0   where  convert(date,brjsrq,23)  between  '2025-02-01'  and  '2025-02-28';
 
 --2.执行对数脚本
 update  a  set  a.isdrg=1 ,a.hi_type=b.insuranceid 
 from   t_setlinfo  a  
 	,( select distinct zylsh,insuranceid from  t_job_settlebillinglist    
-			where convert(date,datebill,23) between  @startTime  and @endTime    and 
+			where convert(date,datebill,23) between  '2025-02-01'  and '2025-02-28'  and 
 			medicalhosid =1  
  	  )  b 
  where a.mdtrt_sn=b.zylsh;
 
 --3.核对结果
 select  distinct  mdtrt_sn from   t_setlinfo  a 
-where  convert(date,brjsrq,23)  between  @startTime  and  @endTime and isdrg =1 ;
+where  convert(date,brjsrq,23)  between  '2025-02-01' and '2025-02-28' and isdrg =1 ;

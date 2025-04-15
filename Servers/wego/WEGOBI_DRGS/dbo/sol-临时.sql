ae8a28b104d640abefe3c63d49093d8badc7fa62
Ysongcao <2804119807@qq.com>
2025-04-15 17:34:34 +0800

    新增报表

---
+++ Servers/wego/WEGOBI_DRGS/dbo/sol-临时.sql
@@ -0,0 +1,21 @@
+SELECT b.mdtrt_sn,a.brjsrq,b.mininf_time ,abs(datediff(day,b.mininf_time,a.brjsrq)) diffdate /*时间差*/
+from (
+/*查询首次上报时间*/
+		SELECT si.mdtrt_sn,convert(date ,min(mr.inf_time),23) mininf_time--,count(*) cb
+		FROM t_setlinfo si 
+		left join t_mihs_result mr on mr.uid=si.mdtrt_sn and mr.err_msg is null
+		where si.isdrg=1 
+			and convert(date ,si.brjsrq,23) between '2025-01-01'and '2025-03-31' /*病人结算时间区间*/
+		GROUP BY si.mdtrt_sn
+		having convert(date ,min(mr.inf_time),23) between '2025-01-01'and '2025-03-31'/*首次上报时间区间*/
+		--having count(*)>1
+)b
+left join (
+/*查询所有病例*/
+		SELECT distinct si.mdtrt_sn,convert(date ,si.brjsrq,23)brjsrq
+		from t_setlinfo si 
+		where si.isdrg=1 
+			and convert(date ,si.brjsrq,23) between '2025-01-01'and '2025-03-31' /*病人结算时间区间*/
+) a on b.mdtrt_sn=a.mdtrt_sn
+where abs(datediff(day,b.mininf_time,a.brjsrq)) <10
+order by diffdate desc

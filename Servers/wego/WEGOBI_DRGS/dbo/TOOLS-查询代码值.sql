a5f89686c0ad9c2f8e519c406f8b3521b9eae306
D.4M._YS_GH <2804119807@qq.com>
2025-04-23 16:07:51 +0800

    规范格式

--- Servers/wego/WEGOBI_DRGS/dbo/TOOLS-查询代码值.sql
+++ Servers/wego/WEGOBI_DRGS/dbo/TOOLS-查询代码值.sql
@@ -1,18 +1,20 @@
 USE DRGS_EXPLAIN;
-DECLARE @QZD  VARCHAR(255)
-      , @QZDe VARCHAR(255);
-SET @QZD = '';
-SET @QZDe = '';
-SELECT ID
-     --, PARENTID AS 父ID
-     --, TABLENAME AS 字段所在表名称
-     , ZDNAME AS 字段名称
-     , HZDNAME AS 字段名称汉字
-     , ZDCODE AS 字段代码
-     , HZDCODE AS 字段代码汉字
-     --, TYPE AS 字段类型
-     --, EZDCODE AS 字段解释
-     , ERRORCODE AS 对应出错列名
-FROM fieldcont2
-WHERE HZDNAME LIKE CONCAT('%', @QZD, '%')
-      OR ERRORCODE LIKE CONCAT('%', @QZDe, '%');
+DECLARE @QZD  VARCHAR(255)  -- 字段代码
+      , @QZDe VARCHAR(255); -- 上报错误代码
+SET @QZD = '';  -- 字段代码
+SET @QZDe = ''; -- 上报错误代码
+SELECT FD2.ID
+     --, FD2.PARENTID                          AS 父ID
+     --, FD2.TABLENAME                         AS 字段所在表名称
+     , COALESCE(FD.TABLENAME, FD2.TABLENAME) AS 字段所在表名称
+     , COALESCE(FD.ZDCODE, FD2.ZDNAME)       AS 字段名称
+     , COALESCE(FD.HZDCODE, FD2.HZDNAME)     AS 字段名称汉字
+     , FD2.ZDCODE                            AS 字段代码
+     , FD2.HZDCODE                           AS 字段代码汉字
+     --, FD2.TYPE                              AS 字段类型
+     --, FD2.EZDCODE                           AS 字段解释
+     , FD2.ERRORCODE                         AS 对应出错列名
+FROM fieldcont2     AS FD2
+LEFT JOIN fieldcont AS FD  ON FD.ID = FD2.PARENTID
+WHERE FD2.HZDNAME 		 LIKE CONCAT('%', @QZD , '%')
+      OR FD2.ERRORCODE LIKE CONCAT('%', @QZDe, '%');

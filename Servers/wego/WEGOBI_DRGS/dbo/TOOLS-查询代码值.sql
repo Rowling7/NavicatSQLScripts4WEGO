157fd7f53fb5cc50aa550b4b2655a93c200da5ef
D.4M._YS_GH <2804119807@qq.com>
2025-04-24 08:59:17 +0800

    修改脚本

--- Servers/wego/WEGOBI_DRGS/dbo/TOOLS-查询代码值.sql
+++ Servers/wego/WEGOBI_DRGS/dbo/TOOLS-查询代码值.sql
@@ -1,20 +1,49 @@
 USE DRGS_EXPLAIN;
-DECLARE @QZD  VARCHAR(255)  -- 字段代码
-      , @QZDe VARCHAR(255); -- 上报错误代码
-SET @QZD = '';  -- 字段代码
-SET @QZDe = ''; -- 上报错误代码
-SELECT FD2.ID
-     --, FD2.PARENTID                          AS 父ID
-     --, FD2.TABLENAME                         AS 字段所在表名称
-     , COALESCE(FD.TABLENAME, FD2.TABLENAME) AS 字段所在表名称
-     , COALESCE(FD.ZDCODE, FD2.ZDNAME)       AS 字段名称
-     , COALESCE(FD.HZDCODE, FD2.HZDNAME)     AS 字段名称汉字
-     , FD2.ZDCODE                            AS 字段代码
-     , FD2.HZDCODE                           AS 字段代码汉字
-     --, FD2.TYPE                              AS 字段类型
-     --, FD2.EZDCODE                           AS 字段解释
-     , FD2.ERRORCODE                         AS 对应出错列名
-FROM fieldcont2     AS FD2
-LEFT JOIN fieldcont AS FD  ON FD.ID = FD2.PARENTID
-WHERE FD2.HZDNAME 		 LIKE CONCAT('%', @QZD , '%')
-      OR FD2.ERRORCODE LIKE CONCAT('%', @QZDe, '%');
+SELECT A.ID
+     , B.TABLENAME AS 字段所在表名称
+     , B.ZDCODE AS 字段名称
+     , B.HZDCODE AS 字段名称汉字
+     , A.ZDCODE AS 字段代码
+     , A.HZDCODE AS 字段代码汉字
+     , B.TYPE AS 字段类型
+     , A.ERRORCODE AS 对应出错列名
+FROM fieldcont2 AS A
+     LEFT JOIN fieldcont AS B ON B.ZDCODE = A.ZDNAME
+WHERE B.HZDCODE LIKE '%%'
+      OR A.ERRORCODE LIKE '%%'
+ORDER BY B.TABLENAME;
+
+
+/*
+SELECT A.ID
+     , D.TABLENAME AS 字段所在表名称
+     , B.ZDNAME       AS 字段名称
+     , B.HZDNAME     AS 字段名称汉字
+     , A.ZDCODE                            AS 字段代码
+     , A.HZDCODE                           AS 字段代码汉字
+     , B.ERRORCODE                         AS 对应出错列名
+FROM fieldcont2     AS A
+LEFT JOIN fieldcont2 AS B ON B.ID = A.PARENTID
+LEFT JOIN fieldcont AS C  ON C.ZDCODE = B.ZDNAME
+LEFT JOIN fieldcont AS D  ON D.ID = C.PARENTID
+
+WHERE B.HZDNAME 		 LIKE '%%'
+      OR B.ERRORCODE LIKE '%%'
+*/
+
+/*
+SELECT B.TABLENAME
+    ,A.ZDNAME
+    ,A.HZDNAME
+    ,C.ZDCODE
+    ,C.HZDCODE
+FROM fieldcont2 A
+full JOIN (
+SELECT X.TABLENAME TABLENAME,X.HTABLENAME HTABLENAME,Y.ZDCODE ZDCODE,Y.HZDCODE HZDCODE
+FROM fieldcont X
+LEFT JOIN fieldcont Y ON Y.PARENTID=X.ID
+WHERE X.TABLENAME IS NOT NULL
+) B ON A.ZDNAME=B.ZDCODE
+left join fieldcont2 c on A.id =c.PARENTID
+WHERE A.PARENTID IS  NULL and A.ZDNAME is not null
+*/

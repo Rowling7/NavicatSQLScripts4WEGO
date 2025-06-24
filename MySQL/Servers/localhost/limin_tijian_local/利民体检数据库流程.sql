0c4497553bf6d8152d515c252e29bacf6004a9ea
D.4M._YS_GH <2804119807@qq.com>
2025-06-24 13:21:39 +0800

    refactor(localhost): 优化查询语句并添加新查询

--- MySQL/Servers/localhost/limin_tijian_local/利民体检数据库流程.sql
+++ MySQL/Servers/localhost/limin_tijian_local/利民体检数据库流程.sql
@@ -77,11 +77,13 @@ limit 100;
 select *
 from t_depart_result_f594102095fd9263b9ee22803eb3f4e5
 where person_id='3f17e041626f4ddcb78186e950f6ad14'
+	and del_flag=0
 limit 100;
 
 select *
 from t_depart_item_result_f594102095fd9263b9ee22803eb3f4e5
-where person_id='3f17e041626f4ddcb78186e950f6ad14'
+where person_id='4a3cf6356d7845aea60aca35c558f806'
+			and  depart_result_id='4e6de6f44fd611f0a48e0242ac110006'
 limit 100;
 
 -- 总检表
@@ -89,3 +91,84 @@ SELECT *
 FROM t_inspection_record_f594102095fd9263b9ee22803eb3f4e5
 where person_id='3f17e041626f4ddcb78186e950f6ad14'
 limit 100;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------------------
+-- 查询订单下的大项目
+SELECT tgo.id, tgo.order_name, tog.name as group_name, togi.name as project_name
+from t_group_order_f594102095fd9263b9ee22803eb3f4e5 tgo
+left JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 tog on tgo.id = tog.group_order_id and tog.del_flag = '0'
+left join t_order_group_item_f594102095fd9263b9ee22803eb3f4e5 togi on tog.id = togi.group_id and togi.del_flag = '0'
+where tgo.order_code = '202506160004'
+  and tgo.del_flag = '0'
+limit 100;
+
+-- 查询该分组下的大项目（组合项目）
+SELECT ogi.*
+FROM t_order_group_item_f594102095fd9263b9ee22803eb3f4e5 ogi
+JOIN t_portfolio_project_f594102095fd9263b9ee22803eb3f4e5 bp
+ON ogi.portfolio_project_id = bp.id and bp.del_flag = 0
+WHERE ogi.group_id IN (
+		SELECT group_id
+		FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
+		WHERE person_name = '陈震南'
+		and del_flag = 0
+		)
+and ogi.del_flag = 0
+LIMIT 100;
+
+-- 查询该分组下每个大项目的子项目（小项目）
+SELECT ogip.*, bsp.name AS sub_project_name
+FROM t_order_group_item_project_f594102095fd9263b9ee22803eb3f4e5 ogip
+JOIN t_base_project_f594102095fd9263b9ee22803eb3f4e5 bsp ON ogip.base_project_id = bsp.id and bsp.del_flag = 0
+WHERE ogip.t_order_group_item_id IN (
+		SELECT id
+		FROM t_order_group_item_f594102095fd9263b9ee22803eb3f4e5
+		WHERE group_id IN (
+				SELECT group_id
+				FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
+				WHERE person_name = '陈震南'
+				and del_flag = 0
+				)
+		)
+and ogip.del_flag = 0
+LIMIT 100;
+
+-- 查询该人员的分诊结果
+SELECT dr.*
+FROM t_depart_result_f594102095fd9263b9ee22803eb3f4e5 dr
+WHERE dr.person_id IN (
+	SELECT id
+	FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
+	WHERE person_name = '陈震南'
+)
+LIMIT 100;
+
+-- 查询该人员具体的体检项目结果
+SELECT dir.*
+FROM t_depart_item_result_f594102095fd9263b9ee22803eb3f4e5 dir
+WHERE dir.person_id IN (
+	SELECT id
+	FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
+	WHERE person_name = '张三'
+)
+LIMIT 100;

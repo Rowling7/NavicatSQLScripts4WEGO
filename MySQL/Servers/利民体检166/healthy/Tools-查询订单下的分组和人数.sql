9d9f5191f2a6072b99097ca81a92d3646d3f4c73
D.4M._YS_GH <2804119807@qq.com>
2025-09-04 17:10:25 +0800

    修改脚本

--- MySQL/Servers/利民体检166/healthy/Tools-查询订单下的分组和人数.sql
+++ MySQL/Servers/利民体检166/healthy/Tools-查询订单下的分组和人数.sql
@@ -4,10 +4,10 @@ SELECT DISTINCT
     og.name AS 分组名称,
 		SUM(og.person_count)AS 分组总人数
 FROM t_group_order_f594102095fd9263b9ee22803eb3f4e5 go
-LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON og.group_order_id = go.id AND go.del_flag <> '1'
-WHERE	og.del_flag<>1
-		and go.del_flag<>1
-    AND go.order_name = '哈工大新生25级'
+LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON og.group_order_id = go.id 
+WHERE	og.del_flag=0
+		and go.del_flag=0
+    AND go.order_name = '哈工大研究生25级'
 GROUP BY go.order_name,og.name WITH ROLLUP
 HAVING GROUPING(go.order_name)=GROUPING(og.name)
 ORDER BY 
--- MySQL/Servers/利民体检166/healthy/log_申请开立.sql
+++ MySQL/Servers/利民体检166/healthy/log_申请开立.sql
@@ -7,18 +7,18 @@ SELECT id,
        SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, '^^^', 1), '|||', -1) AS pName,
 			 left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'PV1||', 1),'|', -1),18) as pIDCARD,
        SUBSTRING_INDEX(CASE
-           WHEN request_param LIKE '%PHYS%' AND request_param LIKE '%ORC|NW|%' THEN SUBSTRING_INDEX(
-                   SUBSTRING_INDEX(request_param, 'ORC|NW|', -1), '|||||||', 1)
-           WHEN request_param LIKE '%PHYS%' AND request_param LIKE '%ORC|CA|%' THEN SUBSTRING_INDEX(
-                   SUBSTRING_INDEX(request_param, 'ORC|CA|', -1), '|||||||', 1)
-           WHEN request_param LIKE '%ECG%'                                     THEN SUBSTRING_INDEX(
-                   SUBSTRING_INDEX(request_param, 'ORC|SC|', -1), '|||F||||', 1)
-           WHEN request_param LIKE '%PACS%'                                    THEN SUBSTRING_INDEX(
-                   SUBSTRING_INDEX(request_param, 'ORC|NW|', -1), '|||||||', 1)
-           WHEN request_param LIKE '%LIS%'                                     THEN SUBSTRING_INDEX(
-                   SUBSTRING_INDEX(request_param, 'ORC|SN|', -1), '|||||||', 1)
+           WHEN request_param LIKE '%PHYS%' AND request_param LIKE '%ORC|NW|%' THEN concat(SUBSTRING_INDEX(
+                   SUBSTRING_INDEX(request_param, 'ORC|NW|', -1), '|||||||', 1),'-','NW')
+           WHEN request_param LIKE '%PHYS%' AND request_param LIKE '%ORC|CA|%' THEN concat(SUBSTRING_INDEX(
+                   SUBSTRING_INDEX(request_param, 'ORC|CA|', -1), '|||||||', 1),'-','CA')
+           WHEN request_param LIKE '%ECG%'                                     THEN concat(SUBSTRING_INDEX(
+                   SUBSTRING_INDEX(request_param, 'ORC|SC|', -1), '|||F||||', 1),'-','SC')
+           WHEN request_param LIKE '%PACS%'                                    THEN concat(SUBSTRING_INDEX(
+                   SUBSTRING_INDEX(request_param, 'ORC|NW|', -1), '|||||||', 1),'-','NW')
+           WHEN request_param LIKE '%LIS%'                                     THEN concat(SUBSTRING_INDEX(
+                   SUBSTRING_INDEX(request_param, 'ORC|SN|', -1), '|||||||', 1),'-','SN')
            ELSE NULL
-           END,'^^',1) AS 申请单号,
+           END,'^^',-1) AS 申请单号,
        response_param AS 响应参数
 FROM t_log_f594102095fd9263b9ee22803eb3f4e5
 WHERE log_type = 2
@@ -34,7 +34,7 @@ WHERE log_type = 2
     WHERE gp.del_flag <> '1'
       AND og.del_flag <> '1'
       AND go.del_flag <> '1'
-      AND go.order_name = '哈工大研究生25级'
+      AND go.order_name = '恒德技工技术学院2025'
 )
 ORDER BY pName;
 

2b7a2766414b5c8e08981b53486ea75a215ec352
D.4M._YS_GH <2804119807@qq.com>
2025-09-10 14:38:02 +0800

    修改名称

--- MySQL/Servers/利民体检166/healthy/log_回传结果_Lis_核对未检.sql
+++ MySQL/Servers/利民体检166/healthy/log_回传结果_Lis_核对未检.sql
@@ -1,16 +1,16 @@
--- Pacs
+-- LIS
 -- 心电图室-90401；影像科-90402；彩超室-90403；病理科-90404；检验科-90405；内镜中心-90120
-DROP TEMPORARY TABLE IF EXISTS t_ECGHL7Log;
-CREATE TEMPORARY TABLE t_ECGHL7Log (
+DROP TEMPORARY TABLE IF EXISTS t_LisHL7Log;
+CREATE TEMPORARY TABLE t_LisHL7Log (
     OrderIdLog VARCHAR(255),
     INDEX idx_OrderIdLog (OrderIdLog)
 ) AS
-SELECT distinct left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'ORC|SC|', -1), '^^', 1),20) AS OrderIdLog
+SELECT distinct left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'ORC|SN|', -1), '|||||||', 1),20) AS OrderIdLog
 FROM t_log_f594102095fd9263b9ee22803eb3f4e5
 WHERE log_type = 2
   AND del_flag = 0
-	and left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'ORC|SC|', -1), '^^', 1),20) like '9%'
-  AND request_param like '%ECG||RIS%';
+	AND left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'ORC|SN|', -1), '|||||||', 1),20) LIKE '9%'
+  AND name = 'LisReceiveHL7Message';
 
 -- 1.LOG 日志部分
 SELECT DISTINCT DENSE_RANK() OVER (ORDER BY patient_id ASC) AS 序号,
@@ -29,9 +29,10 @@ WHERE gp.del_flag <> '1'
   AND og.del_flag <> '1'
   AND go.del_flag <> '1'
   AND dr.del_flag <> '1'
-  AND go.order_code = '202508310001'
-  and dr.office_id in('90401')
-	AND dr.order_application_id not in (SELECT OrderIdLog from t_ECGHL7Log);
+  AND go.order_code = '202508270670'
+  and dr.office_id in('90405')
+	AND dr.order_application_id not in (SELECT OrderIdLog from t_LisHL7Log)
+	and dr.group_item_name <>'γ干扰素释放试验';
 
 -- 2.核对部分
 SELECT gp.test_num AS 体检编号,
@@ -47,4 +48,4 @@ LEFT JOIN t_depart_item_result_f594102095fd9263b9ee22803eb3f4e5 dir ON dr.id = d
 LEFT JOIN t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp ON dr.person_id = gp.id
 WHERE dr.del_flag <> '1'
   AND dir.del_flag <> '1'
-  AND dir.order_application_id = '90001073647';
+  AND dir.order_application_id = '90001076505';

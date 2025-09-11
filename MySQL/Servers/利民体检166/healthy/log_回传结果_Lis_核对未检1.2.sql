df61268e0e1007693685a31780c12bb3e9b5cc11
D.4M._YS_GH <2804119807@qq.com>
2025-09-11 13:37:33 +0800

    refactor(healthy): 更新 SQL 脚本并增加新的查询逻辑
    - 重命名多个 SQL 文件，更新版本号到 1.2
    - 优化现有查询逻辑，调整字段顺序和命名
    - 新增第四部分查询，针对 LOG 表和 result 表都没有回传报文的情况
    - 改

--- MySQL/Servers/利民体检166/healthy/log_回传结果_Lis_核对未检1.2.sql
+++ MySQL/Servers/利民体检166/healthy/log_回传结果_Lis_核对未检1.2.sql
@@ -12,17 +12,18 @@ WHERE log_type = 2
 	AND left(SUBSTRING_INDEX(SUBSTRING_INDEX(request_param, 'ORC|SN|', -1), '|||||||', 1),20) LIKE '9%'
   AND (name = 'LisReceiveHL7Message'OR name = 'ReceiveHL7Message');
 
--- 1.LOG 日志部分
+-- 1. LOG表没有回传报文
 DROP TEMPORARY TABLE IF EXISTS t_OrderIdDRLis;
 CREATE TEMPORARY TABLE t_OrderIdDRLis AS
 	SELECT DISTINCT DENSE_RANK() OVER (ORDER BY patient_id ASC) AS 序号,
-				 dr.order_application_id AS 申请单号,
-				 gp.patient_id AS patient_id,
 				 gp.test_num AS 体检编号,
+				 gp.patient_id AS HIS号,
 				 gp.person_name AS 姓名,
 				 gp.id_card AS 身份证号,
+				 dr.order_application_id AS 申请单号,
 				 dr.office_name AS 科室名称,
-				 dr.group_item_name AS 检测项目
+				 dr.group_item_name AS 检测项目,
+				 NULL AS 'LOG表没有回传报文'
 	FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp
 			LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON gp.group_id = og.id
 			LEFT JOIN t_group_order_f594102095fd9263b9ee22803eb3f4e5 go ON og.group_order_id = go.id
@@ -31,20 +32,20 @@ CREATE TEMPORARY TABLE t_OrderIdDRLis AS
 		AND og.del_flag <> '1'
 		AND go.del_flag <> '1'
 		AND dr.del_flag <> '1'
-		AND go.order_code = '202508270670'
+		AND go.order_code = '202509100002'
 		and dr.office_id in('90405')
 		AND dr.order_application_id not in (SELECT OrderIdLog from t_LisHL7Log)
 		and dr.group_item_name <>'γ干扰素释放试验';
 SELECT * from t_OrderIdDRLis;
 
--- 2.核对结果部分
+-- 2. result表没有结果
 SELECT DISTINCT DENSE_RANK() OVER (ORDER BY gp.patient_id ASC) AS 序号,
 			 gp.test_num AS 体检编号,
        gp.patient_id AS HIS号,
        gp.person_name AS 姓名,
        gp.id_card AS 身份证号,
 			 dr.order_application_id AS 申请单号,
-			        dr.office_name AS 科室名称,
+			 dr.office_name AS 科室名称,
        CASE WHEN gp.is_pass = 1 THEN '登记'
             WHEN gp.is_pass = 2 THEN '在检'
             WHEN gp.is_pass = 3 THEN '总检'
@@ -55,16 +56,16 @@ SELECT DISTINCT DENSE_RANK() OVER (ORDER BY gp.patient_id ASC) AS 序号,
             ELSE NULL
        END AS 收费状态,
        dr.group_item_name AS 检测项目,
-			 '未检' AS 是否检测
+			 NULL AS 'result表没有结果'
 FROM t_depart_result_f594102095fd9263b9ee22803eb3f4e5 dr
 LEFT JOIN t_depart_item_result_f594102095fd9263b9ee22803eb3f4e5 dir ON dr.id = dir.depart_result_id
 LEFT JOIN t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp ON dr.person_id = gp.id
 WHERE dr.del_flag <> '1'
   AND dir.del_flag <> '1'
   AND dir.order_application_id in (SELECT 申请单号 from t_OrderIdDRLis)
-	AND ISNULL(dir.result)<>0;
+	AND ISNULL(dir.result)<>0; 
 
-
+/*
 -- 3.核对详细数据部分
 SELECT gp.test_num AS 体检编号,
        gp.patient_id AS HIS号,
@@ -89,4 +90,34 @@ LEFT JOIN t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp ON dr.person_id = g
 WHERE dr.del_flag <> '1'
   AND dir.del_flag <> '1'
   AND dir.order_application_id in (SELECT 申请单号 from t_OrderIdDRLis)
-	order by 检测结果 desc;
+	-- AND ISNULL(dir.result)=0;
+	order by ISNULL(dir.result) asc;
+*/
+
+
+-- 4.LOG表没有回传报文，result表没有结果
+SELECT DISTINCT DENSE_RANK() OVER (ORDER BY gp.patient_id ASC) AS 序号, 
+				gp.test_num AS 体检编号,
+       gp.patient_id AS HIS号,
+       gp.person_name AS 姓名,
+       gp.id_card AS 身份证号,
+       dr.order_application_id AS 申请单号,
+			 dr.office_name AS 科室名称,
+			 CASE WHEN gp.is_pass = 1 THEN '登记'
+            WHEN gp.is_pass = 2 THEN '在检'
+            WHEN gp.is_pass = 3 THEN '总检'
+            ELSE '已完成'
+       END AS 体检状态,
+       CASE WHEN gp.fee_status = 2 THEN '已退费'
+						when gp.fee_status = -99 THEN '退费中'
+            ELSE NULL
+       END AS 收费状态,
+       dr.group_item_name AS 检测项目,
+			 NULL AS 'LOG表没有回传报文，result表没有结果'
+FROM t_depart_result_f594102095fd9263b9ee22803eb3f4e5 dr
+LEFT JOIN t_depart_item_result_f594102095fd9263b9ee22803eb3f4e5 dir ON dr.id = dir.depart_result_id
+LEFT JOIN t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp ON dr.person_id = gp.id
+WHERE dr.del_flag <> '1'
+  AND dir.del_flag <> '1'
+  AND dir.order_application_id in (SELECT 申请单号 from t_OrderIdDRLis)
+	AND ISNULL(dir.result)<>0; 

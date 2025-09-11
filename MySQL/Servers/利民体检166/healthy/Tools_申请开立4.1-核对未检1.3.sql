0932339401318158e0d3a69ea576a8d1923c69a9
D.4M._YS_GH <2804119807@qq.com>
2025-09-11 14:08:36 +0800

    fix: 修改文件名大小写

--- MySQL/Servers/利民体检166/healthy/Tools_申请开立4.1-核对未检1.3.sql
+++ MySQL/Servers/利民体检166/healthy/Tools_申请开立4.1-核对未检1.3.sql
@@ -1,15 +1,16 @@
 DROP TEMPORARY TABLE IF EXISTS t_OrderIdHL7Log;
 CREATE TEMPORARY TABLE t_OrderIdHL7Log
-(		OrderIdLog VARCHAR(40) ,
+(
+    OrderIdLog VARCHAR(40),
     INDEX idx_OrderIdLog (OrderIdLog)
 ) AS
 SELECT DISTINCT
        SUBSTRING_INDEX(SUBSTRING_INDEX(
-					CASE WHEN request_param LIKE '%ORC|NW|%' THEN SUBSTRING_INDEX(request_param, 'ORC|NW|', -1)
-							 WHEN request_param LIKE '%ORC|CA|%' THEN SUBSTRING_INDEX(request_param, 'ORC|CA|', -1)
-							 ELSE NULL 
-					END
-				, '|||||||', 1), '^^', -1) AS OrderIdLog
+					 CASE WHEN request_param LIKE '%ORC|NW|%' THEN SUBSTRING_INDEX(request_param, 'ORC|NW|', -1)
+								WHEN request_param LIKE '%ORC|CA|%' THEN SUBSTRING_INDEX(request_param, 'ORC|CA|', -1)
+								ELSE NULL
+							 END
+			 , '|||||||', 1), '^^', -1) AS OrderIdLog
 FROM t_log_f594102095fd9263b9ee22803eb3f4e5
 WHERE log_type = 2
   AND del_flag = 0
@@ -17,7 +18,10 @@ WHERE log_type = 2
 
 --  心电图室-90401；影像科-90402；彩超室-90403；病理科-90404；检验科-90405；内镜中心-90120
 SELECT DISTINCT
-       DENSE_RANK() OVER (ORDER BY patient_id ASC) AS 序号,
+       DENSE_RANK() OVER (ORDER BY og.name,patient_id ASC) AS 序号,
+       go.order_code AS 订单号,
+       go.order_name AS 订单名称,
+       og.name AS 分组名称,
        dr.order_application_id AS 申请单号,
        gp.patient_id AS HIS号,
        gp.test_num AS 体检编号,
@@ -29,11 +33,11 @@ SELECT DISTINCT
             WHEN gp.is_pass = 2 THEN '在检'
             WHEN gp.is_pass = 3 THEN '总检'
             ELSE '已完成'
-       END AS 体检状态,
+           END AS 体检状态,
        CASE WHEN gp.fee_status = 2 THEN '已退费'
-						when gp.fee_status = -99 THEN '退费中'
+            WHEN gp.fee_status = -99 THEN '退费中'
             ELSE NULL
-       END AS 收费状态
+           END AS 收费状态
 FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp
     LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON gp.group_id = og.id
     LEFT JOIN t_group_order_f594102095fd9263b9ee22803eb3f4e5 go ON og.group_order_id = go.id
@@ -43,5 +47,6 @@ WHERE gp.del_flag <> '1'
   AND go.del_flag <> '1'
   AND dr.del_flag <> '1'
   AND go.order_code = '202509100002'
-  AND dr.office_id IN ('90401' , '90402' , '90403' , '90404' , '90120')
+  AND dr.office_id IN ('90401', '90402', '90403', '90404', '90120')
   AND dr.order_application_id NOT IN (SELECT OrderIdLog FROM t_OrderIdHL7Log)
+ORDER BY 序号,分组名称;

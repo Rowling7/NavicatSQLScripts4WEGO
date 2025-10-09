a86757077751605baa36a1a6caedade0057b192b
ysongcao <2804119807@qq.com>
2025-10-09 20:56:50 +0800

    1

--- MySQL/Servers/利民体检166/healthy/Tools-批量发申请API.sql
+++ MySQL/Servers/利民体检166/healthy/Tools-批量发申请API.sql
@@ -1,25 +1,29 @@
 -- 第一步：
-SELECT
-	*
-FROM
-	t_group_person_f594102095fd9263b9ee22803eb3f4e5
-WHERE
-	order_id = '2e589ff7efc16d802d33afa48d0ec92a' AND del_flag = '0' AND is_pass = 1
-	
-	
-UPDATE
-	t_group_person_f594102095fd9263b9ee22803eb3f4e5 SET is_pass = 2 ,regist_date = NOW()
-WHERE
-	order_id = '2e589ff7efc16d802d33afa48d0ec92a' AND del_flag = '0' AND is_pass = 1 and PERSON_NAME <>'李红星'
-	
--- 第二步：
-	
-SELECT CONCAT('CALL p_healthy_result_ins(''', test_num, ''', ''f594102095fd9263b9ee22803eb3f4e5'');') as exec_sql
+
+SELECT id, order_code, order_name, group_unit_name
+FROM t_group_order_f594102095fd9263b9ee22803eb3f4e5 go
+WHERE go.order_code = '202510090002';
+
+SELECT *
 FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
-WHERE order_id = '2e589ff7efc16d802d33afa48d0ec92a' 
-AND del_flag = '0' 
-AND is_pass = 2
-and PERSON_NAME <>'李红星';
+WHERE order_id = '10b2916f0ea311fd260b93177d69e97e'
+  AND del_flag = '0'
+  AND is_pass = 1;
 
 
-6eaa3672e5f5d36b087b1d79af68dec4
+-- UPDATE
+    t_group_person_f594102095fd9263b9ee22803eb3f4e5
+SET is_pass = 2,
+    regist_date = NOW()
+-- 	SELECT * from t_group_person_f594102095fd9263b9ee22803eb3f4e5
+WHERE order_id = '10b2916f0ea311fd260b93177d69e97e'
+  AND del_flag = '0'
+  AND is_pass = 1;
+
+-- 第二步：
+
+SELECT CONCAT('CALL p_healthy_result_ins(''', test_num, ''', ''f594102095fd9263b9ee22803eb3f4e5'');') AS exec_sql
+FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
+WHERE order_id = '10b2916f0ea311fd260b93177d69e97e'
+  AND del_flag = '0'
+  AND is_pass = 2;

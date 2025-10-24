9148b04fd3fb778e2cebe6241ecf5a7634a99c6d
D.4M._YS_GH <2804119807@qq.com>
2025-10-24 14:43:17 +0800

    feat(sql): 更新查询逻辑以支持左连接和新订单号
    - 将原有的内连接改为左连接以保留更多数据
    - 更新查询条件中的订单号从202510120

--- MySQL/Servers/localhost/limin_tijian_local/订单名称查询人员信息.sql
+++ MySQL/Servers/localhost/limin_tijian_local/订单名称查询人员信息.sql
@@ -9,14 +9,10 @@ SELECT
        gp.id_card AS 身份证号
 FROM
     t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp
-JOIN
-    t_order_group_f594102095fd9263b9ee22803eb3f4e5 og
-    ON gp.group_id = og.id and og.del_flag<>'1'
-JOIN
-    t_group_order_f594102095fd9263b9ee22803eb3f4e5 go
-    ON og.group_order_id = go.id and go.del_flag<>'1'
+left JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON gp.group_id = og.id and og.del_flag<>'1'
+left JOIN t_group_order_f594102095fd9263b9ee22803eb3f4e5 go ON og.group_order_id = go.id and go.del_flag<>'1'
 WHERE
 		gp.del_flag<>'1'
-    AND go.order_code='202510120001'
+    AND go.order_code='202510150001'
 ORDER BY
     og.name, gp.person_name;

f69faca9f8fe803a90ad8861875fc84508d183b3
D.4M._YS_GH <2804119807@qq.com>
2025-11-07 09:55:01 +0800

    feat(sql): 更新订单号和订单ID查询条件
    - 修改订单号从'202510220001'为'202511060002'
    - 更新订单ID从'360c386d59efac1d056409a3f1696a60'为'02705591e03726d8272fe1e0cf3e0916'
    - 调整t_group_person表查询条件匹配新订单ID
    - 更新p_healthy_result_ins存储过程调用参数匹配新订单ID

--- MySQL/Servers/利民体检166/healthy/Tools-批量发申请API.sql
+++ MySQL/Servers/利民体检166/healthy/Tools-批量发申请API.sql
@@ -2,24 +2,24 @@
 
 SELECT id, order_code, order_name, group_unit_name
 FROM t_group_order_f594102095fd9263b9ee22803eb3f4e5 go
-WHERE go.order_code = '202510220001';-- 订单号
+WHERE go.order_code = '202511060002';-- 订单号
 
 SELECT * -- 重要：必须核对人数
 FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
-WHERE order_id = '360c386d59efac1d056409a3f1696a60'
+WHERE order_id = '02705591e03726d8272fe1e0cf3e0916'
   AND del_flag = '0'
   AND is_pass = 1;
 
 -- 第二步：
 -- UPDATE t_group_person_f594102095fd9263b9ee22803eb3f4e5 
 SET is_pass = 2,regist_date = NOW()
-WHERE order_id = '360c386d59efac1d056409a3f1696a60'
+WHERE order_id = '02705591e03726d8272fe1e0cf3e0916'
   AND del_flag = '0'
   AND is_pass = 1;
 
 -- 第三步：
 SELECT CONCAT('CALL p_healthy_result_ins(''', test_num, ''', ''f594102095fd9263b9ee22803eb3f4e5'');') AS exec_sql 
 FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5
-WHERE order_id = '8ebef809f15aac50ead197945c119bc7'
+WHERE order_id = '02705591e03726d8272fe1e0cf3e0916'
   AND del_flag = '0'
   AND is_pass = 2;

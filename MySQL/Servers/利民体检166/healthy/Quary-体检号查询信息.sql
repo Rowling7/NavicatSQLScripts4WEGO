30b33c5369e1d87f6d55caaae24ae1cfe32cbb0e
D.4M._YS_GH <2804119807@qq.com>
2025-12-11 10:29:08 +0800

    feat(sql): 更新体检查询SQL逻辑
    - 修改字段别名格式统一为AS语法
    - 调整LEFT JOIN连接顺序对齐
    - 更新WHERE条件过滤特定体检号
    - 注释掉科室名称筛选条件
    - 添加恒真条件1=1便于动态SQL拼接
    - 优化ORDER BY子句位置

--- MySQL/Servers/利民体检166/healthy/Quary-体检号查询信息.sql
+++ MySQL/Servers/利民体检166/healthy/Quary-体检号查询信息.sql
@@ -1,26 +1,28 @@
 SELECT DISTINCT
-       go.order_name AS 订单名称,
-       og.name AS 分组名称,
-			 gp.id AS ID,
-       gp.person_name AS 姓名,
-       gp.test_num AS 体检号,
-       gp.patient_id AS HIS号,
-       gp.id_card AS 身份证号,
-       gp.inspection_time AS 导引单打印时间,
-       dr.barcode AS 申请合管号,
-       dr.barname AS 项目名称
-       -- dir.barcode
+    go.order_name AS 订单名称
+     , og.name AS 分组名称
+     , gp.id AS ID
+     , gp.person_name AS 姓名
+     , gp.test_num AS 体检号
+     , gp.patient_id AS HIS号
+     , gp.id_card AS 身份证号
+     , gp.inspection_time AS 导引单打印时间
+     , dr.barcode AS 申请合管号
+     , dr.barname AS 项目名称
+-- dir.barcode
 FROM t_group_person_f594102095fd9263b9ee22803eb3f4e5 gp
-    LEFT JOIN t_depart_result_f594102095fd9263b9ee22803eb3f4e5 dr ON gp.id = dr.person_id
-		LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON gp.group_id = og.id
-		LEFT JOIN t_group_order_f594102095fd9263b9ee22803eb3f4e5 go ON og.group_order_id = go.id
-WHERE gp.del_flag <> '1'
-  AND og.del_flag <> '1'
-  AND go.del_flag <> '1'
-	AND dr.del_flag <> '1'
-	AND gp.test_num='175924507400307'
-			-- and dr.office_name='彩超室'
-order by 申请合管号;
+     LEFT JOIN t_depart_result_f594102095fd9263b9ee22803eb3f4e5 dr ON gp.id = dr.person_id
+     LEFT JOIN t_order_group_f594102095fd9263b9ee22803eb3f4e5 og ON gp.group_id = og.id
+     LEFT JOIN t_group_order_f594102095fd9263b9ee22803eb3f4e5 go ON og.group_order_id = go.id
+WHERE gp.del_flag <> '1' AND
+    og.del_flag <> '1' AND
+    go.del_flag <> '1' AND
+    dr.del_flag <> '1' AND
+    gp.test_num = '176465668100049' AND 
+--  dr.office_name='彩超室' AND
+		1=1
+ORDER BY 申请合管号;
+
 
  
  
